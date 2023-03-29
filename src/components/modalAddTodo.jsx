a325cd2f62ed21bc43fea86c7e8e24a4959efe46
0Gravity94 <leesooyun94@gmail.com>
2023-03-30 06:44:38 +0800

    working on add todo

--- src/components/modalAddTodo.jsx
+++ src/components/modalAddTodo.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { IoIosClose } from "react-icons/io";
+
 function ModalAddTodo() {
   return (
     <div data-cy="todo-add-button">
@@ -12,19 +14,51 @@ function ModalAddTodo() {
       <div data-cy="modal-add">
         <input type="checkbox" id="my-modal-4" className="modal-toggle" />
         <label htmlFor="my-modal-4" className="modal cursor-pointer">
-          <label htmlFor="" className="modal-box">
-            <div>
-              <h1></h1>
-              <span>X</span>
+          <label htmlFor="" className="modal-box p-0 w-100%">
+            <div className="flex justify-between items-center lg:pt-6 lg:px-7">
+              <h1
+                data-cy="modal-add-title"
+                className="text-base lg:text-lg text-Title font-semibold"
+              >
+                Tambah List Item
+              </h1>
+              <label htmlFor="my-modal-4">
+                <IoIosClose
+                  data-cy="modal-add-close-button"
+                  className="text-2xl lg:text-5xl text-Line3 cursor-pointer"
+                />
+              </label>
             </div>
-            <div>
-              <p></p>
-              <input type="text" name="" id="" />
+            <hr />
+            <div className="grid lg:px-7 lg:pt-9">
+              <div className="space-y-3">
+                <p
+                  data-cy="modal-add-name-title"
+                  className="text-xs text-Title font-semibold"
+                >
+                  NAMA LIST ITEM
+                </p>
+                <input
+                  data-cy="modal-add-name-input"
+                  type="text"
+                  placeholder="Tambahkan nama list item"
+                  className="w-full placeholder:text-Line3 border border-BgGray2 rounded-md px-4 py-4 lg:py-3 lg:px-5"
+                />
+              </div>
+              <div className="grid lg:pt-6 lg:pb-6">
+                <p
+                  data-cy="modal-add-priority-title"
+                  className="text-xs text-Title font-semibold"
+                >
+                  PRIORITY
+                </p>
+              </div>
             </div>
-            <div>
+            <hr />
+            <div className="flex items-center justify-end lg:py-4 lg:px-10">
               <label
                 htmlFor="my-modal-4"
-                data-cy="modal-delete-cancel-button"
+                data-cy="modal-add-save-button"
                 className="flex justify-center items-center w-28 h-12 lg:w-36 lg:h-14 bg-BgBlue rounded-full lg:py-3 lg:px-3 text-white cursor-pointer"
               >
                 Simpan
