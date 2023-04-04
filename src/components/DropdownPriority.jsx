ea42966c3791f19dc1d7265d9c6a0f995b31b059
0Gravity94 <leesooyun94@gmail.com>
2023-04-04 15:13:15 +0800

    dropdown

--- src/components/DropdownPriority.jsx
+++ src/components/DropdownPriority.jsx
@@ -1,73 +1,60 @@
-import Select from "react-select";
 import { VscCircleFilled } from "react-icons/vsc";
 import { useState } from "react";
-import IndicatorPriority from "./IndicatorPriorit";
 
 import React from "react";
 
 function DropdownPriority() {
   const [selectedOption, setSelectedOption] = useState(true);
 
-  const options = [
-    {
-      value: "very high",
-      label: (
-        <div className="flex items-center gap-3">
-          <VscCircleFilled className="text-PriorityRed" />
-          <span className="text-base text-Line2">Very High</span>
-        </div>
-      ),
-    },
-    {
-      value: "high",
-      label: (
-        <div className="flex items-center gap-3">
-          <VscCircleFilled className="text-PriorityOrange" />
-          <span className="text-base text-Line2">High</span>
-        </div>
-      ),
-    },
-    {
-      value: "medium",
-      label: (
-        <div className="flex items-center gap-3">
-          <VscCircleFilled className="text-PriorityGreen" />
-          <span className="text-base text-Line2">Medium</span>
-        </div>
-      ),
-    },
-    {
-      value: "low",
-      label: (
-        <div className="flex items-center gap-3">
-          <VscCircleFilled className="text-PriorityBlue" />
-          <span className="text-base text-Line2">Low</span>
-        </div>
-      ),
-    },
-    {
-      value: "very low",
-      label: (
-        <div className="flex items-center gap-3">
-          <VscCircleFilled className="text-PriorityPurple" />
-          <span className="text-base text-Line2">Very Low</span>
-        </div>
-      ),
-    },
-  ];
+  // const options = [
+  //   {
+  //     value: "very high",
+  //     label: (
+  //       <div className="flex items-center gap-3">
+  //         <VscCircleFilled className="text-PriorityRed" />
+  //         <span className="text-base text-Line2">Very High</span>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     value: "high",
+  //     label: (
+  //       <div className="flex items-center gap-3">
+  //         <VscCircleFilled className="text-PriorityOrange" />
+  //         <span className="text-base text-Line2">High</span>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     value: "medium",
+  //     label: (
+  //       <div className="flex items-center gap-3">
+  //         <VscCircleFilled className="text-PriorityGreen" />
+  //         <span className="text-base text-Line2">Medium</span>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     value: "low",
+  //     label: (
+  //       <div className="flex items-center gap-3">
+  //         <VscCircleFilled className="text-PriorityBlue" />
+  //         <span className="text-base text-Line2">Low</span>
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     value: "very low",
+  //     label: (
+  //       <div className="flex items-center gap-3">
+  //         <VscCircleFilled className="text-PriorityPurple" />
+  //         <span className="text-base text-Line2">Very Low</span>
+  //       </div>
+  //     ),
+  //   },
+  // ];
 
-  return (
-    <div data-cy="modal-add-priority-dropdown">
-      {/* <Select className="w-48" value={selectedOption} options={options} /> */}
-
-      <select className="select w-48 h-10 ">
-        <option value="">
-          <IndicatorPriority />
-          Very High
-        </option>
-      </select>
-    </div>
-  );
+  return <div data-cy="modal-add-priority-dropdown" className="dropdown"></div>;
 }
 
 export default DropdownPriority;
