1f35b85254bb06cbfe1492783ba67f10a38a13b1
0Gravity94 <leesooyun94@gmail.com>
2023-04-04 17:38:59 +0800

    state

--- src/components/DetailEmpty.jsx
+++ src/components/DetailEmpty.jsx
@@ -4,10 +4,10 @@ import todoEmptyState from "../assets/todoEmptyState.png";
 function DetailEmpty() {
   return (
     <div
-      data-cy="todo-empty-state"
+      data-cy="item-list-empty-state"
       className="grid justify-items-center lg:flex lg:flex-wrap justify-center mx-5 mt-36 lg:mx-80 lg:mt-16 lg:mb-64 cursor-pointer"
     >
-      <img src={todoEmptyState} alt="list-empty" />
+      <img data-cy="todo-empty-state" src={todoEmptyState} alt="list-empty" />
     </div>
   );
 }
